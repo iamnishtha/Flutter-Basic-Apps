5c37406503230ecf7c5b9473278196296c6e94de
Nishtha Srivastava <51073931+srivastavanishtha@users.noreply.github.com>
2021-06-24 19:10:02 +0530

    Add files via upload

---
+++ Flutter_music_player/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flute_music_player/MusicFinderPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [MusicFinderPlugin registerWithRegistrar:[registry registrarForPlugin:@"MusicFinderPlugin"]];
+}
+
+@end
